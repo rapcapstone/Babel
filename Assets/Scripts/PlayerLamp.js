422e6e8e1d26eca89dd9d4e9801fd78b40d223f9
Roy Kim <1997.roy@gmail.com>
2016-05-17 21:10:55 -0700

    Update on Player Lamp.  - Decaying lamp light intensity  - Notification when oil runs out

--- Assets/Scripts/PlayerLamp.js
+++ Assets/Scripts/PlayerLamp.js
@@ -12,6 +12,9 @@ private var CurrentOil : float = 0;
 private var OilUsage : float = .5;
 private var OilCapacity : float = 100;
 
+private var ShowNotification = false;
+
+public var guiSkin : GUISkin;
 
 function Start () {
     //Initialize lamp oil : 50%
@@ -27,7 +30,6 @@ function Start () {
 }
 
 function Update () {
-    //PlayerLampFlame.GetComponent.<ParticleSystem>().transform.rotation.y = (main camera)transform.rotation.y 
     CurrentOil = Oil;
     //Checks if there is oil.
     if(Oil > 0)
@@ -53,16 +55,25 @@ function Update () {
         LampOff();
     }
     
+    //Burn oil.
     if(State == 1)
     {
         Oil -= Time.deltaTime * OilUsage;
+        IntensityDecay();
+    }
+    
+    //Display message when player runs out of oil.
+    if(State == 1 && Oil < .01)
+    {
+        Notification();
     }
 }
 
 function LampOn() {
     //Enable lamp light.
     PlayerLampLight.GetComponent.<Light>().range = 4.5;
-    PlayerLampLight.GetComponent.<Light>().intensity = 2.5;
+    //PlayerLampLight.GetComponent.<Light>().intensity = 2.5;
+    IntensityDecay();
     PlayerLampFlame.GetComponent.<ParticleSystem>().enableEmission = true;
 }
 
@@ -73,8 +84,50 @@ function LampOff() {
     PlayerLampFlame.GetComponent.<ParticleSystem>().enableEmission = false;
 }
 
+function IntensityDecay() {
+    //Decay light intensity of player lamp as oil decreases.
+    if(Oil < 5)
+    {
+        PlayerLampLight.GetComponent.<Light>().intensity = .5;
+    }
+    else if(Oil < 10)
+    {
+        PlayerLampLight.GetComponent.<Light>().intensity = 1.0;
+    }
+    else if(Oil < 20)
+    {
+        PlayerLampLight.GetComponent.<Light>().intensity = 1.5;
+    }
+    else if(Oil < 30)
+    {
+        PlayerLampLight.GetComponent.<Light>().intensity = 2.0;
+    }
+    else
+    {
+        PlayerLampLight.GetComponent.<Light>().intensity = 2.5;
+    }
+}
+
+function Notification() {
+    //Displays gui label for 2 seconds.
+    ShowNotification = true;
+    yield WaitForSeconds(3);
+    ShowNotification = false;
+}
+
 static function AddOil (amount : int)
 {
     //Oil = Mathf.Clamp(Oil+refill, 0, 100);
     Oil = Oil+refill;
+}
+
+function OnGUI ()
+{
+	if (ShowNotification)
+	{
+		GUI.skin = guiSkin;
+		GUI.Label (Rect (Screen.width/2-100, (Screen.height/2)+175, 200, 50), "You have run out of lamp oil.");
+	
+	}
+
 }
